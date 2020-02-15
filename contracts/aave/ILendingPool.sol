6d71b70d347c076c5412d369b47a9b57a4e53be3
Mikhail Melnik <by.zumzoom@gmail.com>
2020-02-15 11:36:14 +0300

    + aave interfaces

---
+++ contracts/aave/ILendingPool.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0;
+
+contract ILendingPool {
+    function deposit(address _reserve, uint256 _amount, uint16 _referralCode) external payable;
+    function borrow(address _reserve, uint256 _amount, uint256 _interestRateMode, uint16 _referralCode) external;
+    function repay(address _reserve, uint256 _amount, address payable _onBehalfOf) external payable;
+    function flashLoan(address _receiver, address _reserve, uint256 _amount, bytes memory _params) public;
+}
